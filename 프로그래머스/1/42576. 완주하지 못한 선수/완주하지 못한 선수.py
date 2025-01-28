10ad553dfbfa5b19654da6be7e4830eb4e5d6c55
Bumshik Park <79081225+SIKU-KR@users.noreply.github.com>
2025-01-28 14:55:16 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 68.36 ms, Memory: 39 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,7 @@
+from collections import Counter
+
+def solution(participant, completion):
+    participant_counter = Counter(participant)
+    completion_counter = Counter(completion)
+    difference = participant_counter - completion_counter
+    return list(difference.keys())[0]
