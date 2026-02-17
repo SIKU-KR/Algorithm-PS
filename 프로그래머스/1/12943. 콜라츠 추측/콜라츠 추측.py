6e2b241bc25fa8fd333129ce240136eb9e826b5e
Bumshik Park <79081225+SIKU-KR@users.noreply.github.com>
2026-02-17 13:06:47 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.13 ms, Memory: 9.34 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,12 @@
+def solution(num):
+    cnt = 0
+    while num != 1 and cnt < 500:
+        if num % 2 == 0:
+            num /= 2
+        else:
+            num *= 3
+            num += 1
+        cnt += 1
+    if num != 1:
+        return -1
+    return cnt
