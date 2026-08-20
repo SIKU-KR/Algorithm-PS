bcd6b62d554cb271e6824662d1db5d55f652c014
Bumshik Park <79081225+SIKU-KR@users.noreply.github.com>
2026-08-20 14:55:39 +0900

    [level 2] Title: 전화번호 목록, Time: 71.04 ms, Memory: 28.5 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,8 +1,6 @@
-from itertools import combinations
-
 def solution(phone_book):
-    phone_book.sort()  # O(N log N)
-    for i in range(len(phone_book) - 1):  # O(N)
-        if phone_book[i+1].startswith(phone_book[i]):  # O(M)
+    phone_book.sort()    
+    for i in range(0, len(phone_book) - 1):
+        if phone_book[i+1].startswith(phone_book[i]):
             return False
     return True
