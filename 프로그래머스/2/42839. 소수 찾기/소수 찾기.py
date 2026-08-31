40730df5f6d3ebf34c618e138b2d9eb339729641
Bumshik Park <79081225+SIKU-KR@users.noreply.github.com>
2026-08-31 20:55:41 +0900

    [level 2] Title: 소수 찾기, Time: 4.30 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -3,9 +3,10 @@
 # 개수 반환
 
 from itertools import permutations
+import math
 
 def is_sosu(n):
-    for i in range(2, n):
+    for i in range(2, int(math.sqrt(n)) + 1):
         if n % i == 0:
             return False
     return True
