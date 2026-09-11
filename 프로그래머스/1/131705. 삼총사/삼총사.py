b8a967087a137a9ec749ab19fc077035869e7de5
Bumshik Park <79081225+SIKU-KR@users.noreply.github.com>
2026-09-11 17:36:37 +0900

    [level 1] Title: 삼총사, Time: 0.04 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.py
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -1,8 +1,11 @@
 def solution(number):
     answer = 0
-    for i in range(0, len(number)):
-        for j in range(i + 1, len(number)):
-            for k in range(j + 1, len(number)):
-                if number[i] + number[j] + number[k] == 0:
-                    answer += 1
+    
+    for i in range(len(number)):
+        for j in range(i, len(number)):
+            for k in range(j, len(number)):
+                if not (i == j or j == k or k == i):
+                    if number[i] + number[j] + number[k] == 0:
+                        answer += 1
+    
     return answer
